ceded46fcf53f72aec91b886c8dc37d869d28bdb
NazgoooAtanasov <naskodragomirov@gmail.com>
2022-07-02 21:13:41 +0300

    update to port usage

--- installAndStart.sh
+++ installAndStart.sh
@@ -3,4 +3,4 @@
 cd /go/src/app
 #check and install gin if missing i.e. go directory was overriden or mounted
 go get -v github.com/codegangsta/gin
-exec gin "$@"
+exec gin --port 8000 --appPort 8001 "$@"
